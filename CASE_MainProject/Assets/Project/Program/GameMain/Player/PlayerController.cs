ac8764268d38d20085a4ac5f3e17f94a943e37ed
Masatoshi_Kitada <ditgames.inc@gmail.com>
2024-05-01 18:44:56 +0900

    プレイヤーの移動とカメラの追従（Cinemachine）が実装完了

--- CASE_MainProject/Assets/Project/Program/GameMain/Player/PlayerController.cs
+++ CASE_MainProject/Assets/Project/Program/GameMain/Player/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField, Header("移動速度")]
+    [SerializeField, Header("移動加速度")]
     Vector2 runSpeed = new Vector2(1.0f, 1.0f);
+
+    [SerializeField, Header("回転補間速度")]
+    float rotationLerpSpeed = 1.0f;
 
     [SerializeField, Header("ジャンプ力")]
     float jumpPower = 5.0f;
+
+    [SerializeField, Header("移動方向に向く軸（Shaft）")]
+    Transform horizontalRotationShaft;
 
     [SerializeField, Header("蒸気貯蔵量"), ReadOnly]
     float heldSteam = 100.0f;
@@ -17,7 +23,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Header("出力蒸気量"), ReadOnly]
     float outSteam = 0.0f;
 
+    // プレイヤーのRigidbody
     Rigidbody myRigidbody;
+
+    // カメラの前方ベクトルをプレイヤーの進む方向とする
+    Quaternion horizontalRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -28,20 +38,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //移動量入力
-        Vector2 runValue = DualSense_Manager.instance.GetLeftStick() * runSpeed;
+        //=== 回転 ===//
 
-        //フレーム制御
-        runValue *= Time.deltaTime;
+        //カメラの前方ベクトル
+        horizontalRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
 
-        //座標変更
-        this.transform.Translate(runValue.x, 0.0f, runValue.y);
+
+        //=== ジャンプ ===//
 
         //ジャンプ
-        if(DualSense_Manager.instance.GetInputState().CrossButton == DualSenseUnity.ButtonState.Down)
+        if (DualSense_Manager.instance.GetInputState().CrossButton == DualSenseUnity.ButtonState.Down)
         {
             myRigidbody.velocity = transform.up * jumpPower;
         }
+
+        //=== 蒸気出力量 ===//
 
         //蒸気出力
         if (heldSteam > 0.0f)
@@ -52,6 +63,33 @@ public class PlayerController : MonoBehaviour
             {
                 heldSteam = 0.0f;
             }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        //=== 移動 ===//
+
+        //移動量入力
+        Vector2 runValue = DualSense_Manager.instance.GetLeftStick();
+
+        //移動加速度
+        runValue *= runSpeed;
+
+        //フレーム制御
+        runValue *= Time.deltaTime;
+
+        //移動処理
+        myRigidbody.velocity = horizontalRotation * new Vector3(runValue.x, 0.0f, runValue.y);
+
+        //シャフトの向きを変更してプレイヤーを移動方向に向かせる
+        if(runValue.magnitude > 0.1f)
+        {
+            horizontalRotationShaft.localRotation = Quaternion.Lerp(
+                horizontalRotationShaft.localRotation,
+                horizontalRotation,
+                rotationLerpSpeed * Time.deltaTime);
         }
     }
 }
