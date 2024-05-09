eace6e7a22e1c99051eef2f1bf38bac81bea37bf
teru <teru020927@gmail.com>
2024-05-09 20:01:45 +0900

    ステージへの画面遷移完成

--- CASE_MainProject/Assets/Project/Program/Title/TitleSelect.cs
+++ CASE_MainProject/Assets/Project/Program/Title/TitleSelect.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleSelect : MonoBehaviour
 {
+    [SerializeField, Header("アニメーター")]
+    Animator animController;
+
+    [SerializeField, Header("変更するシーン")]
+    public char sceneName;
+
+    private enum title_State
+    {
+        Start,
+        Menu,
+    }
+
+    private title_State scene_now;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scene_now =title_State.Start;  //最初の設定
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(scene_now)
+        {
+            case title_State.Start:
+                titleStart();
+                break;
+            case title_State.Menu:
+                break;
+        }
     }
+
+    void titleStart()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+
 }
