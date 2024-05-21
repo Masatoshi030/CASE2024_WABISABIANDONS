0eeeed81ffad14b2ecb640a6d6f1752ed4d2ed71
kukiokun <sakupiko1103_0610@i.softbank.jp>
2024-05-22 02:40:52 +0900

    ステージセレクトUI仮完成

--- CASE_MainProject/Assets/Project/Program/Select/StageSelect.cs
+++ CASE_MainProject/Assets/Project/Program/Select/StageSelect.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Threading;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StageSelect : MonoBehaviour
 {
+    [SerializeField, Header("クリアステージの数")]
+    int checkNum;
     [SerializeField, Header("クールタイム時間保存")]
     float timeElapsed;
     [SerializeField, Header("UP,DOWNボタン長押しセレクトクールタイム")]
@@ -14,50 +20,44 @@ public class StageSelect : MonoBehaviour
     float noTouchTime;
     [SerializeField, Header("UP,DOWNボタンのクールタイム")]
     float buttonCoolTime;
-
-
     [SerializeField, Header("ページ設定")]
     GameObject[] papers;
 
     //定義
-    int nowPage;    //今のページ
-    int nowSelect;  //今のセレクトステージ
-    int maxPage = 4;    //ページの最大
-    int minPage = 0;    //ページの最小
-    int maxSelectStage = 6;     //ステージの最大
-    int minSelectStage = 0;     //ステージの最小
-    public GameObject[,] stageArray = new GameObject[4, 6];     //全ステージ数の配列
+    static int clearPage;                //現在のクリアページ
+    static int clearSelectPage;    //現在のクリアセレクト数
+    static int nowPage;     //今のページ
+    static int nowSelect;  //今のセレクトステージ
+    static int maxPage = 4;    //ページの最大
+    static int minPage = 0;     //ページの最小
+    static int maxSelectStage = 6;     //ステージの最大
+    static int minSelectStage = 0;     //ステージの最小
 
-    // Start is called before the first frame update
+    public GameObject[,] stageArray = new GameObject[4, 6];     //全ステージ数の配列
+    public GameObject[,] checkArray = new GameObject[4, 6];     //ステージのクリア状況確認配列
+
+    // Start関数
     void Start()
     {
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 6; j++)
-            {
-                stageArray[i, j] = papers[i].transform.GetChild(j).gameObject;
-            }
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Active状態以外は非表示にする処理
+        //ステージのセレクトUI格納
         for (int i = minPage; i < maxPage; i++)
         {
             for (int j = minSelectStage; j < maxSelectStage; j++)
             {
-                if (stageArray[i, j] != null)
-                {
-
-                }
+                stageArray[i, j] = papers[i].transform.GetChild(j).transform.GetChild(1).gameObject;
+                checkArray[i, j] = papers[i].transform.GetChild(j).transform.GetChild(0).gameObject;
+                checkArray[i, j].SetActive(true);
             }
         }
+    }
 
+    // Update関数
+    void Update()
+    {
         //ページセレクトスクリプトから今のページを持ってくる
         nowPage = PageSelect.serectPage;
 
+        //触っていない時間格納
         noTouchTime += Time.deltaTime;
 
         //下ボタン処理
@@ -80,6 +80,7 @@ public class StageSelect : MonoBehaviour
                 timeElapsed = 0.0f;
             }
 
+            //上限に達したら上に戻る処理
             if (nowSelect > maxSelectStage - 1)
             {
                 nowSelect = minSelectStage;
@@ -110,7 +111,7 @@ public class StageSelect : MonoBehaviour
                 nowSelect--;
                 timeElapsed = 0.0f;
             }
-
+            //上限に達したら下に戻る処理
             if (nowSelect < minSelectStage)
             {
                 nowSelect = maxSelectStage - 1;
@@ -119,13 +120,63 @@ public class StageSelect : MonoBehaviour
             //ボタンが触られたのでクールタイム初期化
             noTouchTime = 0.0f;
         }
+
+        //セレクトされていない状態にする処理
+        for (int i = minPage; i < maxPage; i++)
+        {
+            for (int j = minSelectStage; j < maxSelectStage; j++)
+            {
+                if (stageArray[i, j] != null)
+                {
+                    stageArray[i, j].SetActive(false);
+                }
+            }
+        }
+        //セレクトされているボタンをActiveにする処理
         SelectActive(nowPage, nowSelect);
+
+        //チェックを表示する
+        //チェックが全体のステージ数を越えていないか判定する処理
+        if (checkNum >= 0 && maxPage * maxSelectStage >= checkNum)
+        {
+            clearPage = checkNum / maxSelectStage;
+            clearSelectPage = checkNum % maxSelectStage;
+
+            //越えている場合(クリアステージが6以上)
+            if (clearPage > 0)
+            {
+                for (int i = 0; i < clearPage; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        checkArray[i, j].GetComponent<Animator>().SetBool("bCheck", true);
+                        checkArray[i, j].SetActive(true);
+                    }
+                    for (int j = 0; j < clearSelectPage; j++)
+                    {
+                        checkArray[clearPage, j].GetComponent<Animator>().SetBool("bCheck", true);
+                        checkArray[clearPage, j].SetActive(true);
+                    }
+                }
+            }
+            else  //越えていない場合(クリアステージが6以上)
+            {
+                for (int i = 0; i < clearSelectPage; i++)
+                {
+                    checkArray[clearPage, i].GetComponent<Animator>().SetBool("bCheck", true);
+                    checkArray[clearPage, i].SetActive(true);
+                }
+            }
+        }
+        else if (checkNum > 24)     //24ステージ以上を選択してしまった場合
+        {
+            checkNum = 24;
+        }
     }
 
-    //セレクトされているボタンをActiveにする処理
+    //セレクトされているフレームをActiveにする処理
     private void SelectActive(int _nowPage,int _nowSelect)
     {
-        stageArray[_nowPage, _nowSelect].transform.GetChild(0).GetComponent<Animator>().SetBool("bCheck", true);
-        stageArray[_nowPage, _nowSelect].transform.GetChild(0).gameObject.SetActive(true);
+        stageArray[_nowPage, _nowSelect].SetActive(true);
     }
 }
