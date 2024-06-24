12a6ff91c9c3b8dbe00f9489cd0e6a5faa486c33
hqrse <adaharuto8610@gmail.com>
2024-06-24 11:20:01 +0900

    A-3調整
    変更内容
    ---
    檜山先生のフィードバックを元にA-3を修正
    バルブの自動取得の補間開始位置をその場になるように設定
    自動取得の開始が早くなったことにつきバルブの拡散を強くなるように設定

--- CASE_MainProject/Assets/Project/Program/GameMain/Enemy/ValveEmitter.cs
+++ CASE_MainProject/Assets/Project/Program/GameMain/Enemy/ValveEmitter.cs
@@ -12,11 +12,11 @@ public class ValveEmitter : MonoBehaviour
         // 30度刻みで生成する
         float originVelocitySpeed = velocitySpeed;
         float subAngle = 0.0f;
-        float upVelocityPower = 2.0f;
+        float upVelocityPower = 7.5f;
         float sideVelocityPower = velocitySpeed;
 
         // 1.1秒で取り切れるようにする(プレイヤーまでの距離補完含め(2.1秒))
-        float duration = 0.1f;
+        float duration = 0.2f;
         float timerAdd = maxGetTime / spawnNum;
         if(isAuto)
         {
@@ -27,8 +27,9 @@ public class ValveEmitter : MonoBehaviour
                 transform.Rotate(Vector3.up, angle);
                 GameObject obj = Instantiate(instanceObject, transform.position + (Vector3.up * 3.5f), Quaternion.identity);
                 Vector3 force = transform.forward;
+                force *= velocitySpeed;
                 force.y += upVelocityPower;
-                obj.GetComponent<Rigidbody>().AddForce(force * velocitySpeed, ForceMode.Impulse);
+                obj.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
                 AutoValveGet autoValve = obj.GetComponent<AutoValveGet>();
                 autoValve.waitTime = duration;
                 autoValve.isAuto = true;
