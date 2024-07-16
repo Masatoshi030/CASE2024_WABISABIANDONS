7027e983206d97799f8dfec4ceeb3cf7360fe3db
hqrse <hqrse0818@gmail.com>
2024-07-17 02:20:06 +0900

    デスクトップ→ノート引継ぎ用

--- CASE_MainProject/Assets/Project/Program/GameMain/Player/HeadCollision_Manager.cs
+++ CASE_MainProject/Assets/Project/Program/GameMain/Player/HeadCollision_Manager.cs
@@ -28,7 +28,7 @@ public class HeadCollision_Manager : MonoBehaviour
 
     //重複防止　前回衝突したオブジェクト
     string lastCollisionEnterObjectName = "";
-    GameObject lastCollisionEnterObject;
+    string lastCollisionEnterObjectTag = "";
 
 
     // Start is called before the first frame update
@@ -47,14 +47,14 @@ public class HeadCollision_Manager : MonoBehaviour
     {
 
         //同じオブジェクトに複数回衝突した場合は
-        if(other.gameObject.name == lastCollisionEnterObjectName && lastCollisionEnterObject.tag != "Enemy")
+        if(other.gameObject.name == lastCollisionEnterObjectName && lastCollisionEnterObjectTag != "Enemy")
         {
             return;
         }
         else
         {
             // 衝突したオブジェクトを記録
-            lastCollisionEnterObject = other.gameObject;
+            lastCollisionEnterObjectTag = other.gameObject.tag;
             //衝突したオブジェクト名を記録
             lastCollisionEnterObjectName = other.gameObject.name;
         }
