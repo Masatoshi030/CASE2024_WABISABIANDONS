6a56c247b62286743743b7bc82f6dde45b1d619a
hqrse <adaharuto8610@gmail.com>
2024-07-19 15:26:38 +0900

    チュートリアル全実装完了

--- CASE_MainProject/Assets/MessageJudgeCollision.cs
+++ CASE_MainProject/Assets/MessageJudgeCollision.cs
@@ -8,10 +8,12 @@ public class MessageJudgeCollision : MonoBehaviour
 
     [SerializeField, Header("このチュートリアルを表示する判定オブジェクト")]
     MessageElementController messageElementController;
+    [SerializeField, Header("判定対象のタグ")]
+    string targetTag = "Player";
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == targetTag)
         {
             if(!bHited)
             {
